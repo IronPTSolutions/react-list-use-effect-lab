35fddd3de621896b472080d5a6947e9ae3e9b6f3
Carla Lopez <iamcarla.28@gmail.com>
2024-11-30 10:35:25 +0100

    Second iteration

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,13 @@
+import PageLayout from "../components/ui/layouts/page-layout/page-layouts";
+
 function Home () {
     return ( 
+        <PageLayout>
         <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia dolore praesentium consequatur atque? Fuga incidunt blanditiis voluptatum hic ut voluptate dolore tempore suscipit cumque iste minima placeat porro, necessitatibus cum.
         </p>
-    )
+        </PageLayout>
+    );
 }
 
 export default Home; 

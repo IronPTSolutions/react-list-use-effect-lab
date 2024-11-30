7866b89a250f5c05e7e4c59b654369125e3e0c6b
Carla Lopez <iamcarla.28@gmail.com>
2024-11-30 13:00:36 +0100

    Input nombre

--- src/components/contacts/contact-list/ContactList.jsx
+++ src/components/contacts/contact-list/ContactList.jsx
@@ -5,13 +5,13 @@ import contactData from "../../../data/users.json";
 function ContactList ({ className = "", name}) {
     const [contacts, setContacts ] = useState ([]);
 
-    useEffect(()=> {
+    useEffect(() => {
         const filteredContacts = name? 
-            contactData.filter((contact) => contact.name === name):
+            contactData.filter((contact) => contact.name.toLowerCase().includes(name?.toLowerCase())):
             contactData;
 
         setContacts (filteredContacts)
-    }, []);
+    }, [name]);
 
     const handleContactDeletion = (contact) => {
         const filteredContacts = contacts.filter((c) => c.id !== contact.id);

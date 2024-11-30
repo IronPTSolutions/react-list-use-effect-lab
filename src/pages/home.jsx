309c1edb7301f640cdc52236789e647787ab69e6
hurtaditoo <al415507@uji.es>
2024-11-30 10:21:10 +0100

    Iteración 1: Crear la Página Home

---
+++ src/pages/home.jsx
@@ -0,0 +1,8 @@
+
+function Home() {
+  return (
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero autem magnam error voluptas aliquam, neque laudantium deserunt culpa, velit sed eligendi perspiciatis blanditiis inventore, quibusdam deleniti? Tenetur harum asperiores ratione!</p>
+  )
+}
+
+export default Home;

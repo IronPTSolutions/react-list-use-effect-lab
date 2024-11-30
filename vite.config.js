d6812611b9143ca5e3cfd82fa5061fb9a42c7f21
Carlos del Prado <carlos.delpradomota@telefonica.com>
2024-11-30 09:02:44 +0100

    fix: github paths

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-list-use-effect-lab',
+  base: '/react-list-use-effect-lab/',
   plugins: [react()],
 })

52f0a571784a759e7bd91062208122f1c35e1907
David Rojas <dmrojassantana@gmail.com>
2024-11-30 11:07:02 +0100

    iteracion 1

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  );
+}
+
+export default Home;

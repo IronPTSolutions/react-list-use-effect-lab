38a16514d0618203de5154969ea69f59c17d0fb7
David Rojas <dmrojassantana@gmail.com>
2024-11-30 13:28:48 +0100

    done

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import ContactItem from "./ContactItem";
 import usersData from "../data/users.json";
 
-const ContactList = () => {
+const ContactList = ({ filter }) => {
   const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
@@ -13,10 +13,14 @@ const ContactList = () => {
     setContacts(contacts.filter(contact => contact.id !== id));
   };
 
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <div>
       <h2>Lista de contactos</h2>
-      {contacts.map((contact) => (
+      {filteredContacts.map((contact) => (
         <ContactItem 
           key={contact.id} 
           contact={contact} 
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import { useState } from 'react';
 import ContactList from '../ContactList';
 
 function Home() {
+  const [filter, setFilter] = useState('');
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
   return (
     <div>
       <h1>Home Page</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      <ContactList /> {}
+      <input
+        type="text"
+        placeholder="Filtrar contactos por nombre"
+        value={filter}
+        onChange={handleFilterChange}
+      />
+      <ContactList filter={filter} />
     </div>
   );
 }

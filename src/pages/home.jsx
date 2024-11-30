ac9b1a6696de2ec744ae52e11c889afcae81b66d
hurtaditoo <al415507@uji.es>
2024-11-30 10:31:24 +0100

    Iteración 2: Componente PageLayout

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,10 @@
+import { PageLayout } from "../components/layouts";
 
 function Home() {
   return (
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero autem magnam error voluptas aliquam, neque laudantium deserunt culpa, velit sed eligendi perspiciatis blanditiis inventore, quibusdam deleniti? Tenetur harum asperiores ratione!</p>
+    <PageLayout>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero autem magnam error voluptas aliquam, neque laudantium deserunt culpa, velit sed eligendi perspiciatis blanditiis inventore, quibusdam deleniti? Tenetur harum asperiores ratione!</p>
+    </PageLayout>
   )
 }
 

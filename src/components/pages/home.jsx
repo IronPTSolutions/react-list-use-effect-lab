6f866b07f177e9614e46d0e3435f8cc2df57cc55
Blancadum <blancadum@gmail.com>
2024-11-30 11:08:53 +0100

    it1

---
+++ src/components/pages/home.jsx
@@ -0,0 +1,17 @@
+
+
+function Home(){
+    return (
+        <>
+        <Container>
+            <h1>Holi APP</h1>
+            <p>Lorem fistrum amatomaa qué dise usteer apetecan diodenoo. Ahorarr te voy a borrar el cerito te voy a borrar el cerito te va a hasé pupitaa a peich caballo blanco caballo negroorl. De la pradera tiene musho peligro pecador de la pradera la caidita te voy a borrar el cerito quietooor por la gloria de mi madre ahorarr por la gloria de mi madre me cago en tus muelas. Me cago en tus muelas a peich diodeno ese hombree sexuarl a peich la caidita. Pecador no puedor por la gloria de mi madre diodeno apetecan. Apetecan de la pradera ese pedazo de diodenoo no puedor papaar papaar. 
+            </p>
+        </Container>
+        
+        </>
+    )
+}
+
+
+export default Home;
